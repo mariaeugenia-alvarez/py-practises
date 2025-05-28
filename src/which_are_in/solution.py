1c31fdb80322d4a51a76ed4cb410e3cc627ccb3b
Maru <mariaeu.alvarez@gmail.com>
2025-05-28 14:12:20 +0200

    Refactor solution to improve efficiency by using set comprehension and string joining

--- src/which_are_in/solution.py
+++ src/which_are_in/solution.py
@@ -1,9 +1,4 @@
 def solution(array1, array2):
-    final_list= []
-    for i in array1:
-        for j in array2:
-            if (i in j) and (i not in final_list):
-                final_list.append(i)
-                
-    sort_list=sorted(final_list)
-    return (sort_list)
+    concatenated_array2 = " ".join(array2)
+    result = sorted({i for i in array1 if i in concatenated_array2})
+    return (result)
